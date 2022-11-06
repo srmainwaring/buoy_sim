12651a2186b882c64ca18f3f0a995aad776d0ede
Andrew Hamilton <hamilton@mbari.org>
2022-11-06 14:41:09 -0800

    Adding Complex amplitude computation and RAO to FS_Hydrodynamics

--- buoy_gazebo/src/FreeSurfaceHydrodynamics/FS_Hydrodynamics.hpp
+++ buoy_gazebo/src/FreeSurfaceHydrodynamics/FS_Hydrodynamics.hpp
@@ -1,4 +1,3 @@
-
 // Copyright 2022 Open Source Robotics Foundation, Inc. and Monterey Bay Aquarium Research Institute
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,105 +11,134 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#ifndef FREESURFACEHYDRODYNAMICS__FS_HYDRODYNAMICS_HPP_
+#define FREESURFACEHYDRODYNAMICS__FS_HYDRODYNAMICS_HPP_
 
 #include <vector>
-#include <Eigen/Dense>
+#include <string>
+
 #include "IncidentWave.hpp"
+#include <Eigen/Dense>
 
-using namespace Eigen;
-
-#define STORAGE_MULTIPLIER 5 // storage space for xddot and eta.  Should be >2, larger takes more memory but there's less data shuffling
+// Storage space for xddot and eta.
+//  Should be >2, larger takes more memory but there's less data shuffling
+#define STORAGE_MULTIPLIER 5
 
 class FS_HydroDynamics
 {
 public:
-    //FS_HydroDynamics();
-    FS_HydroDynamics(IncidentWave &IncWave);
-    FS_HydroDynamics(IncidentWave &IncWave, double L, double g, double rho);
-    //void AssignIncidentWave(IncidentWave &IncWave);
-    void ReadWAMITData_FD(std::string filenm);
-    void ReadWAMITData_TD(std::string filenm);
-    void Plot_FD_Coeffs();
-    void Plot_TD_Coeffs();
-    double AddedMass(double omega, int i, int j);
-    double Damping(double omega, int i, int j);
+  // FS_HydroDynamics();
+  explicit FS_HydroDynamics(IncidentWave & IncWave);
+  FS_HydroDynamics(IncidentWave & IncWave, double L, double g, double rho);
+  // void AssignIncidentWave(IncidentWave &IncWave);
+  void ReadWAMITData_FD(std::string filenm);
+  void ReadWAMITData_TD(std::string filenm);
+  void Plot_FD_Coeffs();
+  void Plot_TD_Coeffs();
 
-    void SetTimestepSize(double dt);
-    double GetTimestepSize();
-    void SetWaterplane(double S, double S11, double S22);
-    void SetCOB(double x, double y, double z);
-    void SetVolume(double V);
+  double AddedMass(double omega, int i, int j);
+  Eigen::Matrix<double, 6, 6> AddedMass(double omega);
 
-    Eigen::VectorXd BuoyancyForce(Eigen::VectorXd x);
-    Eigen::VectorXd RadiationForce(Eigen::VectorXd last_xddot);
-    Eigen::VectorXd ExcitingForce();
+  double Damping(double omega, int i, int j);
+  Eigen::Matrix<double, 6, 6> Damping(double omega);
 
-    void WaveExcitingForceComponents(double *XiRe, double *XiIm, double omega, int j);
+  std::complex<double> WaveExcitingForceComponents(double omega, int j);
+  Eigen::Matrix<std::complex<double>,6,1> WaveExcitingForceComponents(double omega);
 
-    friend std::ostream &
-    operator<<(std::ostream &out, const FS_HydroDynamics &f);
-    std::string m_fd_filename;
-    std::string m_td_filename;
-    IncidentWave &_IncWave;
-    double m_L = 1;
-    double m_grav = 9.81;
-    double m_rho = 1025;
+  void SetTimestepSize(double dt);
+  double GetTimestepSize();
 
-    // Frequency domain coefficients
-    VectorXd fd_am_dmp_tps;
-    VectorXd fd_am_dmp_omega;
-    std::vector<Eigen::Matrix<double, 6, 6>> fd_X;
-    std::vector<Eigen::Matrix<double, 6, 6>> fd_Y;
-    Eigen::Matrix<double, 6, 6> fd_X_inf_freq;
-    Eigen::Matrix<double, 6, 6> fd_Y_inf_freq;
-    Eigen::VectorXd fd_ext_tps;
-    Eigen::VectorXd fd_ext_omega;
-    Eigen::VectorXd fd_ext_beta;
-    std::vector<Eigen::Matrix<double, Dynamic, 1>> fd_Mod_Xi;
-    std::vector<Eigen::Matrix<double, Dynamic, 1>> fd_Pha_Xi;
-    std::vector<Eigen::Matrix<double, Dynamic, 1>> fd_Re_Xi;
-    std::vector<Eigen::Matrix<double, Dynamic, 1>> fd_Im_Xi;
+  void SetWaterplane(double S, double S11, double S22);
+  void SetCOB(double x, double y, double z);
+  void SetCOG(double x, double y, double z);
+  void SetVolume(double V);
+  void SetMass(double m);
+  void SetI(Eigen::Matrix<double,3,3> I);
 
-    // Time domain coefficients
-    VectorXd m_tau_rad;
-    Eigen::Matrix<VectorXd, 6, 6> m_IR_cosint;
-    Eigen::Matrix<VectorXd, 6, 6> m_IR_sinint;
+  Eigen::VectorXd BuoyancyForce(Eigen::VectorXd x);
+  Eigen::VectorXd RadiationForce(Eigen::VectorXd last_xddot);
+  Eigen::VectorXd ExcitingForce();
 
-    double m_dtau_exc;
-    VectorXd m_tau_exc;
-    Eigen::Matrix<VectorXd, 1, 6> m_IR_exc;
+  Eigen::Matrix<std::complex<double>, 6, 1> ComplexAmplitude(double omega);
 
-    // Vectors for numerical integration
-    double m_dt = 0;
-    Eigen::Matrix<VectorXd, 6, 6> m_L_rad;
-    Eigen::Matrix<VectorXd, 1, 6> m_L_exc;
+  friend std::ostream &
+  operator<<(std::ostream & out, const FS_HydroDynamics & f);
+  std::string m_fd_filename;
+  std::string m_td_filename;
+  IncidentWave & _IncWave;
+  double m_L = 1;
+  double m_grav = 9.81;
+  double m_rho = 1025;
 
-    // Storage for accelerations for each of 6 DOF
-    Eigen::Matrix<VectorXd, 1, 6> m_xddot;
+  // Frequency domain coefficients
+  Eigen::VectorXd fd_am_dmp_tps;
+  Eigen::VectorXd fd_am_dmp_omega;
+  std::vector<Eigen::Matrix<double, 6, 6>> fd_A;  // Note: fd_A is a_ij in Newman pp 295, eqn 152
+  std::vector<Eigen::Matrix<double, 6, 6>> fd_B;  // Note: fd_B is b_ij in Newman pp 295, eqn 152
+  Eigen::Matrix<double, 6, 6> fd_A_inf_freq;
+  Eigen::Matrix<double, 6, 6> fd_B_inf_freq;
+  Eigen::VectorXd fd_ext_tps;
+  Eigen::VectorXd fd_ext_omega;
+  Eigen::VectorXd fd_ext_beta;
+  std::vector<Eigen::Matrix<double, Eigen::Dynamic, 1>> fd_Mod_Chi;
+  std::vector<Eigen::Matrix<double, Eigen::Dynamic, 1>> fd_Pha_Chi;
+  std::vector<Eigen::Matrix<double, Eigen::Dynamic, 1>> fd_Re_Chi;
+  std::vector<Eigen::Matrix<double, Eigen::Dynamic, 1>> fd_Im_Chi;
 
-    // Storage for wave-elevation at origin;
-    VectorXd _eta0;
-    
-    int _rad_tstep_index = 0;
-    int _exc_tstep_index = 0;
-    int _n_rad_intpts = 0;
-    int _n_exc_intpts = 0;
+  Eigen::Matrix<double, 6, 6> c; //Hydrostatics that depend upon body position
 
-    // wave-elevation evaluation time;
-    double _t_eta;
+  // Time domain coefficients
+  Eigen::VectorXd m_tau_rad;
+  Eigen::Matrix<Eigen::VectorXd, 6, 6> m_IR_cosint;
+  Eigen::Matrix<Eigen::VectorXd, 6, 6> m_IR_sinint;
 
-    /// \brief Buoy WaterPlane Area
-    double S;  
+  double m_dtau_exc;
+  Eigen::VectorXd m_tau_exc;
+  Eigen::Matrix<Eigen::VectorXd, 6, 1> m_IR_exc;
 
-    /// \brief Buoy WaterPlane Area Second Moment of Area around x
-    double S11;  
+  // Vectors for numerical integration
+  double m_dt = 0;
+  Eigen::Matrix<Eigen::VectorXd, 6, 6> m_L_rad;
+  Eigen::Matrix<Eigen::VectorXd, 6, 1> m_L_exc;
 
-    /// \brief Buoy WaterPlane Area Second Moment of Area around y
-    double S22;  
+  // Storage for accelerations for each of 6 DOF
+  Eigen::Matrix<Eigen::VectorXd, 6, 1> m_xddot;
 
-    /// \brief Submerged Volume 
-    double Vol;
+  // Storage for wave-elevation at origin;
+  Eigen::VectorXd _eta0;
 
-    /// \brief Center of Buoyancy relative to water-plane coordinate system 
-    Eigen::Vector3d COB;
+  int _rad_tstep_index = 0;
+  int _exc_tstep_index = 0;
+  int _n_rad_intpts = 0;
+  int _n_exc_intpts = 0;
+
+  // wave-elevation evaluation time;
+  double _t_eta;
+
+  /// \brief Buoy WaterPlane Area
+  double S;
+
+  /// \brief Buoy WaterPlane Area Second Moment of Area around x
+  double S11;
+
+  /// \brief Buoy WaterPlane Area Second Moment of Area around y
+  double S22;
+
+  /// \brief Submerged Volume
+  double Vol;
+
+  /// \brief Center of Buoyancy relative to water-plane coordinate system
+  Eigen::Vector3d COB;
+
+  /// \brief Center of gravity relative to water-plane coordinate system
+  Eigen::Vector3d COG;
+  
+  /// \brief Mass Matrix, required only for complex amplitude calculation
+  Eigen::Matrix<double, 6, 6> M;
+
+  private:
+  void Compute_cij(void);
+
 };
+
+#endif  // FREESURFACEHYDRODYNAMICS__FS_HYDRODYNAMICS_HPP_
--- buoy_gazebo/src/FreeSurfaceHydrodynamics/Test_FS_Hydrodynamics.cpp
+++ buoy_gazebo/src/FreeSurfaceHydrodynamics/Test_FS_Hydrodynamics.cpp
@@ -17,62 +17,66 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
 
 #include <Eigen/Dense>
-#include "gnuplot-iostream.h"
 #include "FS_Hydrodynamics.hpp"
-
-//#include "EtaFunctor.hpp"
 #include "LinearIncidentWave.hpp"
-//#include <boost/tuple/tuple.hpp>
 #include "gnuplot-iostream.h"
 
 int main()
 {
-  const char *modes[6] = {"Surge", "Sway", "Heave", "Roll", "Pitch", "Yaw"};
+  const char * modes[6] = {"Surge", "Sway", "Heave", "Roll", "Pitch", "Yaw"};
   LinearIncidentWave Inc;
-  LinearIncidentWave &IncRef = Inc;
+  LinearIncidentWave & IncRef = Inc;
   FS_HydroDynamics BuoyA5(IncRef, 1.0, 9.81, 1025);
-  BuoyA5.SetWaterplane(5.47,1.37,1.37); // Set area and 2nd moments of area for waterplane
-  BuoyA5.SetCOB(0,0,-.18); //Set COB relative to waterplane coordinate system.
-  BuoyA5.SetVolume(1.75); 
+  BuoyA5.SetWaterplane(5.47, 1.37, 1.37);  // Set area and 2nd moments of area for waterplane
+  BuoyA5.SetCOB(0, 0, -.18);  // Set COB relative to waterplane coordinate system.
+  BuoyA5.SetVolume(1.75);
   BuoyA5.ReadWAMITData_FD("HydrodynamicCoeffs/BuoyA5");
   BuoyA5.ReadWAMITData_TD("HydrodynamicCoeffs/BuoyA5");
   BuoyA5.Plot_FD_Coeffs();
   BuoyA5.SetTimestepSize(.01);
   BuoyA5.Plot_TD_Coeffs();
 
-#if 1 // Test Radiation Forces
-  srand((unsigned)time(0));
-  double A = .5; //.5 + ((float)(rand() % 20) / 10);
-  double T = 8; //3.0 + (rand() % 9);
-  double tf = 3 * T;
+  std::srand((unsigned)time(0));
+  double A = .5;  // .5 + ((float)(std::rand() % 20) / 10);
+  double T = 4;  // 3.0 + (std::rand() % 9);
+  double tf = 2.0 * T;
   double omega = 2 * M_PI / T;
-  int first = 1;
-  for (int i = 0; i < 6; i++)   // i determines mode of motion.
-    for (int j = 0; j < 6; j++) // j denotes direction of resulting force
-    {
-      double am = BuoyA5.AddedMass(omega, i, j);
-      double dmp = BuoyA5.Damping(omega, i,  j);
-      double am_inf = BuoyA5.fd_X_inf_freq(i, j);
+  double phase = 20*M_PI/180;
+  double dt = 0.01;
 
-      std::vector<double> pts_t,pts_F_TD,pts_F_FD;
-      std::vector<double> pts_vel,pts_accel;
+#if 1  // Test Radiation Forces
+// Note:  This computes the radiation forces for each mode of motion indivdually, so 
+// MemRadiation is called 6 times per timestep, once with each acceleration set non-zero
+// In use it will be called only once per time-step, with all acclerations set.
+{
+  int first = 1;
+  for (int i = 0; i < 6; i++) {  // i determines mode of motion.
+    for (int j = 0; j < 6; j++) {  // j denotes direction of resulting force
+      double am = BuoyA5.AddedMass(omega, i, j);
+      double dmp = BuoyA5.Damping(omega, i, j);
+      double am_inf = BuoyA5.fd_A_inf_freq(i, j);
+
+      std::vector<double> pts_t, pts_F_TD, pts_F_FD;
+      std::vector<double> pts_vel, pts_accel;
       double last_accel = 0;
       double F_max = -std::numeric_limits<double>::max();
       double F_min = std::numeric_limits<double>::max();
-      for (int k = 0; k < tf / BuoyA5.m_dt; k++)
-      {
-        double tt = BuoyA5.m_dt * k;
+      BuoyA5.SetTimestepSize(dt);  // Reset timestep to re-initialize storage in BuoyA5 Class
+      for (int k = 0; k < tf / dt; k++) {
+        double tt = dt * k;
         pts_t.push_back(tt);
-        //double pos = A * cos(omega * tt);  //Not needed, but handy to see...
-        double vel = -A * omega * sin(omega * tt);
+        // double pos = A * cos(omega * tt);  // Not needed, but handy to see...
+        double vel = -A * omega * sin(omega * tt+phase);
         pts_vel.push_back(vel);
-        double accel = -A * pow(omega, 2) * cos(omega * tt);
+        double accel = -A * pow(omega, 2) * cos(omega * tt + phase);
         pts_accel.push_back(accel);
         Eigen::VectorXd xddot(6);
-        for (int n = 0; n < 6; n++)
+        for (int n = 0; n < 6; n++) {
           xddot(n) = 0;
+        }
         xddot(i) = last_accel;
 
         Eigen::VectorXd MemForce(6);
@@ -80,77 +84,75 @@ int main()
         pts_F_TD.push_back(am_inf * accel + MemForce(j));
         last_accel = accel;
         double FD_Force = am * accel + dmp * vel;
-        pts_F_FD.push_back(am * accel + dmp * vel); // FD_Force);
-        if (FD_Force > F_max)
+        pts_F_FD.push_back(FD_Force);
+        if (FD_Force > F_max) {
           F_max = FD_Force;
-        if (FD_Force < F_min)
+        }
+        if (FD_Force < F_min) {
           F_min = FD_Force;
+        }
       }
-      if(first)
-      {
+      if (first) {
         Gnuplot gp;
         first = 0;
         char Amp[10];
-        sprintf(Amp, "%.1f", A);
+        snprintf(Amp, sizeof(Amp), "%.1f", A);
         char Per[10];
-        sprintf(Per, "%.1f", T);
+        snprintf(Per, sizeof(Per), "%.1f", T);
         gp << "set term X11 title  'A = " << Amp << "m  T = " << Per << "s'\n";
         gp << "set grid\n";
         gp << "set xlabel 'time (s)'\n";
-        gp << "plot '-' w l title 'Vel'"
-           << ",'-' w l title 'Accel'\n";
+        gp << "plot '-' w l title 'Vel'" <<
+          ",'-' w l title 'Accel'\n";
         gp.send1d(boost::make_tuple(pts_t, pts_vel));
         gp.send1d(boost::make_tuple(pts_t, pts_accel));
       }
-      if ((F_min < -1) && (F_max > 1)) // Don't plot near-zero forces
-      {
+      if ((F_min < -1) && (F_max > 1)) {  // Don't plot near-zero forces
         Gnuplot gp;
         char Amp[10];
-        sprintf(Amp, "%.1f", A);
+        snprintf(Amp, sizeof(Amp), "%.1f", A);
         char Per[10];
-        sprintf(Per, "%.1f", T);
-        gp << "set term X11 title 'Radiation Forces: " << modes[i] << " Motions, " << modes[j] << " Forces:  A = " << Amp << "m  T = " << Per << "s  \n";
+        snprintf(Per, sizeof(Per), "%.1f", T);
+        gp << "set term X11 title 'Radiation Forces: " << modes[i] << " Motions, " << modes[j] <<
+          " Forces:  A = " << Amp << "m  T = " << Per << "s  \n";
         gp << "set grid\n";
         gp << "set xlabel 'time (s)'\n";
-        if (j < 3)
+        if (j < 3) {
           gp << "set ylabel 'F (N)'\n";
-        else
+        } else {
           gp << "set  ylabel 'M (N-m)'\n";
-        gp << "plot '-' w l title 'Time-Domain'"
-           << ",'-' w l title 'Freq-Domain'\n";
+        }
+        gp << "plot '-' w l title 'Time-Domain'" <<
+          ",'-' w l title 'Freq-Domain'\n";
         gp.send1d(boost::make_tuple(pts_t, pts_F_TD));
         gp.send1d(boost::make_tuple(pts_t, pts_F_FD));
         gp << "set xlabel 'time (s)'\n";
-        if (j < 3)
+        if (j < 3) {
           gp << "set ylabel 'F (N)'\n";
-        else
+        } else {
           gp << "set  ylabel 'M (N-m)'\n";
-        gp << "set title '" << modes[i] << "(t) = " << std::fixed << std::setprecision(1) << Amp << "cos(2 pi t/" << Per << ")'  \n";
+        }
+        gp << "set title '" << modes[i] << "(t) = " << std::fixed << std::setprecision(1) << Amp <<
+          "cos(2 pi t/" << Per << ")'  \n";
         gp << "replot\n";
       }
     }
+  }
+}
 #endif
 
-#if 0 // Test Exciting Forces
-  srand((unsigned)time(0));
-  double A = .5 + ((float)(rand() % 20) / 10);
-  double T = 3.0 + (rand() % 9);
-  double tf = 5 * T;
-  double omega = 2 * M_PI / T;
+#if 1  // Test Exciting Forces
+{
+  Inc.SetToMonoChromatic(2 * A, T, phase, 180*M_PI/180);
 
-  //Inc.SetToPiersonMoskowitzSpectrum(2*A, 0);
-  Inc.SetToMonoChromatic(2*A, T, 0);
-  double XiRe, XiIm;
-
-  for (int j = 0; j < 6; j++) // j denotes direction of resulting force
-  {
-    BuoyA5.WaveExcitingForceComponents(&XiRe, &XiIm, Inc.m_omega[0], j);
+  for (int j = 0; j < 6; j++) {  // j denotes direction of resulting force
+    std::complex<double> Chi = BuoyA5.WaveExcitingForceComponents(Inc.m_omega[0], j);
     std::vector<double> pts_t, pts_F_TD, pts_F_FD, pts_eta;
-    for (int k = 0; k < tf / BuoyA5.m_dt; k++)
-    {
-      double tt = BuoyA5.m_dt * k;
+    BuoyA5.SetTimestepSize(dt);
+    for (int k = 0; k < tf / dt; k++) {
+      double tt = dt * k;
       pts_t.push_back(tt);
-      pts_F_FD.push_back(Inc.m_A[0] * XiRe * cos(omega * tt) - Inc.m_A[0] * XiIm * sin(omega * tt));
+      pts_F_FD.push_back(Inc.m_A[0] * Chi.real() * cos(omega * tt + phase*cos(180*M_PI/180)) - Inc.m_A[0] * Chi.imag() * sin(omega * tt + phase*cos(180*M_PI/180)));
       pts_eta.push_back(Inc.eta(0, 0, tt));
       Eigen::VectorXd ExtForce(6);
       ExtForce = BuoyA5.ExcitingForce();
@@ -160,68 +162,80 @@ int main()
     gp << "set term X11 title  '" << modes[j] << " Exciting Forces'\n";
     gp << "set grid\n";
     gp << "set xlabel 'time (s)'\n";
-    if (j < 3)
+    if (j < 3) {
       gp << "set ylabel 'F (N)'\n";
-    else
+    } else {
       gp << "set  ylabel 'M (N-m)'\n";
-    gp << "plot '-' w l title 'Time-Domain'"
-       << ",'-' w l title 'Freq-Domain'"
-       << ",'-' w l title 'eta(t)'\n";
+    }
+    gp << "plot '-' w l title 'Time-Domain'" <<
+      ",'-' w l title 'Freq-Domain'" <<
+      ",'-' w l title 'eta(t)'\n";
     gp.send1d(boost::make_tuple(pts_t, pts_F_TD));
     gp.send1d(boost::make_tuple(pts_t, pts_F_FD));
     gp.send1d(boost::make_tuple(pts_t, pts_eta));
     gp << "set xlabel 'time (s)'\n";
-    if (j < 3)
+    if (j < 3) {
       gp << "set ylabel 'F (N)'\n";
-    else
+    } else {
       gp << "set  ylabel 'M (N-m)'\n";
+    }
     gp << "replot\n";
   }
+}
 #endif
 
-#if 0 // Test Buoyancy Forces
-  srand((unsigned)time(0));
-  double A = .5 + ((float)(rand() % 20) / 10);
-  double T = 6.0 + (rand() % 9);
-  double tf = T;
-  double omega = 2 * M_PI / T;
-
-
-  for (int j = 0; j < 6; j++) // j denotes direction of resulting force
-  {
-    std::vector<double> pts_t, pts_x,pts_F_B;
-    for (int k = 0; k < tf / BuoyA5.m_dt; k++)
-    {
-      double tt = BuoyA5.m_dt * k;
+#if 1  // Test Buoyancy Forces
+{
+  for (int j = 0; j < 6; j++) {  // j denotes direction of resulting force
+    std::vector<double> pts_t, pts_x, pts_F_B;
+    for (int k = 0; k < tf / dt; k++) {
+      double tt = dt * k;
       pts_t.push_back(tt);
       Eigen::VectorXd x(6);
       x(0) = 0; x(1) = 0; x(2) = 0;x(3) = 0; x(4) = 0; x(5) = 0;
-      x(j) = A*cos(omega*tt);
+      x(j) = A * cos(omega * tt+phase);
       pts_x.push_back(x(j));
       Eigen::VectorXd BuoyancyForce(6);
       BuoyancyForce = BuoyA5.BuoyancyForce(x);
-      std::cout << "x = " << x.transpose() << std::endl;
-      std::cout << "F_B = " << BuoyancyForce.transpose() << std::endl << std::endl;
       pts_F_B.push_back(BuoyancyForce(j));
     }
     Gnuplot gp;
     gp << "set term X11 title  '" << modes[j] << " Buoyancy Forces'\n";
     gp << "set grid\n";
     gp << "set xlabel 'time (s)'\n";
-    if (j < 3)
+    if (j < 3) {
       gp << "set ylabel 'F (N)'\n";
-    else
+    } else {
       gp << "set  ylabel 'M (N-m)'\n";
-    gp << "plot '-' w l title 'x(t)'"
-       << ",'-' w l title 'Buoyancy Force'\n";
+    }
+    gp << "plot '-' w l title 'x(t)'" <<
+      ",'-' w l title 'Buoyancy Force'\n";
     gp.send1d(boost::make_tuple(pts_t, pts_x));
     gp.send1d(boost::make_tuple(pts_t, pts_F_B));
     gp << "set xlabel 'time (s)'\n";
-    if (j < 3)
+    if (j < 3) {
       gp << "set ylabel 'F (N)'\n";
-    else
+    } else {
       gp << "set  ylabel 'M (N-m)'\n";
+    }
     gp << "replot\n";
   }
+}
+#endif
+
+#if 1  //Test Motions
+{
+
+BuoyA5.SetMass(1400);
+Eigen::Matrix<double,3,3> I;
+ I <<  400,   0,   0, 
+         0, 400,   0,
+         0,   0, 200;
+BuoyA5.SetI(I);
+
+std::cout << "Complex Amplitudes" << std::endl;
+std::cout << BuoyA5.ComplexAmplitude(omega) << std::endl;
+
+}
 #endif
 }
